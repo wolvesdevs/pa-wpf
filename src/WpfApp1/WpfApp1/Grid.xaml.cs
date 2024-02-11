ac0761c317547d84ec0289ab37e4f36174445b87
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-02-11 17:06:01 +0900

    refs #04 コントロールの名前の付け方

--- src/WpfApp1/WpfApp1/Grid.xaml.cs
+++ src/WpfApp1/WpfApp1/Grid.xaml.cs
@@ -15,13 +15,15 @@ using System.Windows.Shapes;
 namespace WpfApp1
 {
     /// <summary>
-    /// _3_Grid.xaml の相互作用ロジック
+    /// Grid.xaml の相互作用ロジック
     /// </summary>
-    public partial class _3_Grid : Window
+    public partial class Grid : Window
     {
-        public _3_Grid()
+        public Grid()
         {
             InitializeComponent();
+
+            ResultLabel.Content = "12345";
         }
     }
 }
