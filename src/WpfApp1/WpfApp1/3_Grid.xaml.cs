cb840485b638c76a0bf77c9f4c1e3d25e65e46a2
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-02-11 16:54:31 +0900

    refs #03 Grid

---
+++ src/WpfApp1/WpfApp1/3_Grid.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// _3_Grid.xaml の相互作用ロジック
+    /// </summary>
+    public partial class _3_Grid : Window
+    {
+        public _3_Grid()
+        {
+            InitializeComponent();
+        }
+    }
+}
