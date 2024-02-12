728d31bc51c6f07e868c48e726e1ca356af08689
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-02-12 22:46:28 +0900

    refs #10_2 ListView

--- src/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ src/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using SQLite;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,22 +19,24 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<Customer> _customers = new();
+        private ObservableCollection<Customer> _customers = new();
+        private int _index = 0;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            _customers.Add(new Customer { Id = 1, Name = "name1", Phone = "phone1" });
-            _customers.Add(new Customer { Id = 2, Name = "name2", Phone = "phone2" });
-            _customers.Add(new Customer { Id = 3, Name = "name3", Phone = "phone3" });
+            _customers.Add(new Customer { Id = ++_index, Name = $"name{_index}", Phone = $"phone{_index}" });
+            _customers.Add(new Customer { Id = ++_index, Name = $"name{_index}", Phone = $"phone{_index}" });
+            _customers.Add(new Customer { Id = ++_index, Name = $"name{_index}", Phone = $"phone{_index}" });
 
             CustomerListView.ItemsSource = _customers;
         }
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-
+            _customers.Add(new Customer { Id = ++_index, Name = $"name{_index}", Phone = $"phone{_index}" });
+            CustomerListView.ItemsSource = _customers;
         }
 
         //private void SaveButton_Click(object sender, RoutedEventArgs e)
