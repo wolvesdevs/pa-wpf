c8bdf4f8c929e6f3d532b14679f8c504d288d798
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-02-13 23:29:52 +0900

    refs #13 Button

---
+++ src/WpfApp1/WpfApp1/SubWindow1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1.UI
+{
+    /// <summary>
+    /// SubWindow1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class SubWindow1 : Window
+    {
+        public SubWindow1()
+        {
+            InitializeComponent();
+        }
+
+        private void NormalButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("NormalButton is clicked!!");
+        }
+
+        private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine($"{DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")} RepeatButton");
+        }
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine($"toggle button click {ToggleButton.IsChecked}");
+        }
+    }
+}
