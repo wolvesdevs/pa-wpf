687b72988985ac32b63c047da80b0ad522565437
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-02-15 00:24:14 +0900

    refs #18 Slider

---
+++ src/WpfApp1/WpfApp1/Views/Slider.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1.UI.Views
+{
+    /// <summary>
+    /// Slider.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Slider : Window
+    {
+        public Slider()
+        {
+            InitializeComponent();
+        }
+
+        private void ASlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ASlider.Text = e.NewValue.ToString();
+        }
+
+        private void BSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            BSlider.Text = Math.Round(e.NewValue, 0).ToString();
+        }
+
+    }
+}
