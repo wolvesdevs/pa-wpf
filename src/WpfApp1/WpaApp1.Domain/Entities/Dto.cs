e3e3e33796d98f91152a08728266cee9534b52c6
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-02-16 00:03:41 +0900

    refs #21 ListBox

---
+++ src/WpfApp1/WpaApp1.Domain/Entities/Dto.cs
@@ -0,0 +1,8 @@
+﻿namespace WpaApp1.Domain.Entities
+{
+    public class Dto(string fileName, string name)
+    {
+        public string FileName { get; set; } = fileName;
+        public string Name { get; set; } = name;
+    }
+}
