ea6243a5e907c02f59b26624eff3afb143e4947a
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-02-16 17:00:15 +0900

    refs #25 MenuItem

---
+++ src/WpfApp1/WpfApp1/Views/Menu.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1.UI.Views
+{
+    /// <summary>
+    /// Menu.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
+    }
+}
